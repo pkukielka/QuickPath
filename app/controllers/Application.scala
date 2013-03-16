86621b7500d4d9992ce059550804bbeb0a88e418
Piotr Kukielka <piotr.kukielka@gmail.com>
2013-03-16 18:44:32 +0100

    Add Server actor class - proper design

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,18 +7,11 @@ import play.api.libs.json._
 import play.api.libs.iteratee._
 
 object Application extends Controller {
-	val hubEnum = Enumerator.imperative[JsValue]()
-	val hub = Concurrent.hub[JsValue](hubEnum)
-
   def index = Action {
     Ok(views.html.index())
   }
 
-  def stream = WebSocket.async[JsValue] {
-  	request =>
-      val out = hub.getPatchCord()
-      val in = Iteratee.foreach[JsValue] (hubEnum push _)
-
-  		Promise.pure((in, out))
+  def stream = WebSocket.async[JsValue] { request =>
+    models.Server.join()
 	}
 }
--- app/models/Server.scala
+++ app/models/Server.scala
@@ -1,27 +1,68 @@
 package models
 
 import akka.actor.Actor
-import play.api.libs.iteratee.{Enumerator, Concurrent}
+import akka.actor.Props
+import akka.pattern.ask
+import akka.util.Timeout
+
+import scala.concurrent.duration._
+
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.iteratee._
 import play.api.libs.json._
+import play.api.Play.current
+import play.libs.Akka
 
 sealed abstract class ServerMessage
-case class Event     (event:      JsValue)             extends ServerMessage
-case class Join      (username:   String)              extends ServerMessage
-case class Connected (enumerator: Enumerator[JsValue]) extends ServerMessage
+case class GetUniqueId  ()                                                       extends ServerMessage
+case class Join         (username: String, channel: Concurrent.Channel[JsValue]) extends ServerMessage
+case class Event        (username: String, event: JsValue)                       extends ServerMessage
+case class Quit         (username: String)                                       extends ServerMessage
+
+object Server {
+  implicit val timeout = Timeout(1 second)
+  lazy val serv = Akka.system.actorOf(Props[Server])
+
+  def join(): scala.concurrent.Future[(Iteratee[JsValue,_], Enumerator[JsValue])] = {
+    (serv ? GetUniqueId()).map {
+      case userId: Int =>
+        val username   = userId.toString
+        val enumerator = Concurrent.unicast[JsValue](serv ! Join(username, _))
+        val iteratee   = Iteratee.foreach[JsValue](serv ! Event(username, _)).mapDone(_ => serv ! Quit(username))
+        (iteratee, enumerator)
+
+      case _ =>
+        val iteratee   = Done[JsValue,Unit]((),Input.EOF)
+        val enumerator =
+          Enumerator[JsValue](Json.obj("error" -> "Server cannot create unique user name")) >>>
+          Enumerator.enumInput(Input.EOF)
+        (iteratee,enumerator)
+    }
+  }
+}
 
 class Server extends Actor {
   var players = Map.empty[String, Concurrent.Channel[JsValue]]
+  var id: Int = 0
 
   def receive = {
-    case Join(username) =>
-      // Concurrent.unicast returns iteratee.Enumerator[E] to the sender
-      sender ! Connected(Concurrent.unicast[JsValue](
-        onStart = channel => players = players + (username -> channel),
-        onComplete = players = players - username
+    case GetUniqueId() =>
+      sender ! {id += 1; id}
+
+    case Join(username, channel) =>
+      players = players + (username -> channel)
+
+    case Event(username, event) =>
+      players.values.foreach(_.push(
+        Json.obj(
+          "username" -> username,
+          "x" -> (event \ "x"),
+          "y" -> (event \ "y"),
+          "z" -> (event \ "z")
+        )
       ))
 
-    case Event(e) => {
-      players.values.foreach(_.push(e))
-    }
+    case Quit(username) =>
+      players = players - username
   }
 }
